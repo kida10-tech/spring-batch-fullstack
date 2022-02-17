9db1d8969c6f5ebacf6405f95035d3404c274888
Diego Martinez Giardini <diego.martinez.giardini@gmail.com>
2022-02-17 20:16:51 +0100

    MatchController updated. MatchPage done

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -5,9 +5,26 @@ import { MatchSmallCard } from "../components/MatchSmallCard";
 
 export const MatchPage = () => {
 
-  return (
+    const [matches, setMatches] = useState([]);
+    const { teamName, year } = useParams();
+
+    useEffect(
+        () => {
+            const fetchMatches = async () => {
+                const response = await fetch(`http://localhost:8080/api/team/${teamName}/matches?year=${year}`);
+                const data = await response.json();
+                setMatches(data);
+            };
+            fetchMatches();
+        }, []
+     )
+
+  return (  
     <div className="MatchPage">
       <h1>MatchPage</h1>
+      {
+          matches.map(match => <MatchDetailCard teamName={teamName} match={match} />)
+      }
     </div>
   );
 };
